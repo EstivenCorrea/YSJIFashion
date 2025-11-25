fb0faa8751295546ecfbeaaa5846dfb87c7cd677
Estiven Correa <correaestiven09@gmail.com>
2025-11-24 23:13:36 -0500

    db

--- ysjifashion/settings.py
+++ ysjifashion/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': 'ysji_crud',           # el nombre de tu base de datos
         'USER': 'root',           # tu usuario de MySQL
         'PASSWORD': '',           # tu contraseña (vacía si no tienes)
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
